07ec1f39daf44e4f7084049df220ea9d8b734432
Ajiteru Dolapo <83882048+Dol-Desh@users.noreply.github.com>
2024-01-08 13:10:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// //Javascript code for changing the style of each tab
+// const allTabs = document.querySelectorAll(".tab-button");
+// const allArticle = document.querySelectorAll(".article-list");
+
+// allTabs.forEach((tab, index) =>{
+
+//     tab.addEventListener("click", (e)=>{
+
+//         allTabs.forEach(tab=>{
+//             tab.classList.remove("active-tab");});
+//         tab.classList.add("active-tab");
+//     })
+
+//     allArticle.forEach(content =>{
+//         content.classList.remove("active");
+//     });
+//     // allArticle[index].classList.add("active");
+// })
+
+// //Javascript code for changing the contents of tabs as the tab changes
+
+
+const allTabs = document.querySelectorAll(".tab-button");
+const allArticles = document.querySelectorAll(".article-list");
+
+allTabs.forEach((tab, index) => {
+    tab.addEventListener("click", (e) => {
+        // Remove "active-tab" class from all tabs
+        allTabs.forEach((tab) => {
+            tab.classList.remove("active-tab");
+        });
+
+        // Add "active-tab" class to the clicked tab
+        tab.classList.add("active-tab");
+
+        // Hide all articles
+        allArticles.forEach((article) => {
+            article.classList.remove("active");
+        });
+
+        // Show the corresponding article
+        allArticles[index].classList.add("active");
+    });
+});
