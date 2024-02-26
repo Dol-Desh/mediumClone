d6a2cc13109a7f00e26515d27590740da8771f98
Ajiteru Dolapo <83882048+Dol-Desh@users.noreply.github.com>
2024-02-26 17:59:17 +0100

    backend

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,34 +3,39 @@ const router = express.Router();
 import Post from "../models/post.js";
 
 //routes
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     const locals = {
         title: "nodeJS blog",
     };
-    res.render("index.ejs", locals);
+
+    try {
+        const data = await Post.find();
+        res.render("index.ejs", {locals, data});   
+    } catch (error) {
+        console.log(error);
+    }
 });
 
-function insertPostData() {
-    Post.insertMany([
-        {
-            title: "First backend",
-            body: "This is my first dummy test",
-        },
+// function insertPostData() {
+//     Post.insertMany([
+//         {
+//             title: "First backend",
+//             body: "This is my first dummy test",
+//         },
 
-        {
-            title: "second backend",
-            body: "This is my second dummy test",
-        },
+//         {
+//             title: "second backend",
+//             body: "This is my second dummy test",
+//         },
 
-        {
-            title: "Third backend",
-            body: "This is my third dummy test",
-        }
-    ], { timeout: 30000 }, { batchSize: 100 });
-}
+//         {
+//             title: "Third backend",
+//             body: "This is my third dummy test",
+//         }
+//     ], { timeout: 30000 }, { batchSize: 100 });
+// }
 
-
-insertPostData();
+// insertPostData();
 
 router.get("/home", (req, res) => {
     res.render("home.ejs");
@@ -43,5 +48,6 @@ router.get("/article", (req, res) => {
 router.get("/write", (req, res) => {
     res.render("write.ejs");
 });
+
 
 export default router;
